9a0bad3ac1948bc6fe11b39534ed3248cf09cc75
John <mjohncorbet@gmail.com>
2023-05-07 20:38:28 +0800

    setup image carousel

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-//  set header paddingLeft dependent from hero margin;
+//  set header paddingLeft equal to hero margin;
 const heroTextContainer = document.querySelector('.text-container');
 const distanceFromLeft = heroTextContainer.offsetLeft;
 const logo = document.querySelector('.logo');
 logo.style.paddingLeft = `${distanceFromLeft}px`;
+
+const slideButtons = document.querySelectorAll('[data-carousel-button]');
+
+slideButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselButton === 'next'? 1 : -1;
+        const slides = document
+            .querySelector('.carousel')
+            .querySelector('[data-slides]');
+
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+
+        const titlePositive = document.querySelector('.title-positive');
+        if (newIndex === 0) titlePositive.style.color = '#371A1D';
+        if (newIndex === 1) titlePositive.style.color = 'rgba(255, 255, 255, 0.8)';
+        if (newIndex > 1) titlePositive.style.color = '#CFAD9D';
+    })
+});
